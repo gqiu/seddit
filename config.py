04e64f1fbf2aef42a64bb74720dd4b6b4ae8a031
dnewberry <dnewberry@23b8b987-112f-0410-a829-35e0f83341f8>
2008-07-31 16:50:33 +0000

    doing some backup stuff as I reset my slice. wow this was a long time ago.
    git-svn-id: http://seddit.googlecode.com/svn/trunk@123 23b8b987-112f-0410-a829-35e0f83341f8

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import web
 
 # connect to the database
-web.config.db_parameters = dict(dbn='postgres', db='seddit_remix', user='dnewberry', pw='')
+web.config.db_parameters = dict(dbn='postgres', db='seddit_development', user='dnewberry', pw='')
 
 web.webapi.internalerror = web.debugerror
 middleware = [web.reloader]
